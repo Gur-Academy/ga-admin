6603b257eb020d2e3432e69ec26950cbd26b90cd
codebyjsa <jsaishere@gmail.com>
2025-06-16 11:41:28 +0530

    setup

---
+++ src/main/java/com/guracademy/GaAdmin/GaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.guracademy.GaAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaAdminApplication.class, args);
+	}
+
+}
