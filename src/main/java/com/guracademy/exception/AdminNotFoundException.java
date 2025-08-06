2e01959162976261ff0e6efc42bb0c3cbb88e42a
codebyjsa <jsaishere@gmail.com>
2025-08-06 12:15:51 +0530

    Project Init

--- src/main/java/com/guracademy/exception/AdminNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.guracademy.exception;
-
-import java.util.UUID;
-
-public class AdminNotFoundException extends RuntimeException{
-    public AdminNotFoundException(UUID adminId) {
-        super("Admin with ID " + adminId + " not found");
-    }
-}
